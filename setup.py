6eccd31b40b089694074ff7fde85664876948aad
yehonatanz <yonti95@gmail.com>
2018-11-17 22:45:20 +0200

    Add long description and url to package

--- setup.py
+++ setup.py
@@ -2,15 +2,23 @@ import os
 
 from setuptools import find_packages, setup
 
-PACKAGE_DIR = os.path.join(os.path.dirname(__file__), 'konst')
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def _open(*path):
+    return open(os.path.join(PROJECT_DIR, *path))
 
 
 def _read_version():
-    with open(os.path.join(PACKAGE_DIR, '__init__.py'), 'r') as f:
+    with _open('konst', '__init__.py') as f:
         for line in f:
             if line.startswith('__version__'):
-                return line.split('=')[-1].strip().strip('\'"')
-        raise ValueError('Could not find __version__')
+                return line.split('=')[-1].strip().strip('\'')
+
+
+def _read(*path):
+    with _open(*path) as f:
+        return f.read()
 
 
 def main():
@@ -18,11 +26,13 @@ def main():
         name='konst',
         version=_read_version(),
         description='Effortless, meaningless, DRY constants',
+        long_description=_read('README.rst'),
         packages=find_packages(exclude=['tests']),
         python_requires=">=3.6",
         extras_require={
             'dev': ['isort', 'black', 'pytest', 'pytest-watch', 'pytest-cov']
         },
+        url='https://github.com/yehonatanz/konst',
         classifiers=(
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python :: 3.6',
